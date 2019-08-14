27849b23d3fa0415f0a1929b3cfa9d77cb726bf0
Chris Tram <ctram@protonmail.com>
2019-08-14 00:27:15 -0700

    create singleton web_app_setting model if does not exist

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -4,6 +4,13 @@ class StaticsController < ApplicationController
   skip_before_action :verify_user_logged_in, only: [:sign_in]
 
   def index
+    @web_app_settings = WebAppSetting.first
+
+    unless @web_app_settings
+      @web_app_settings = WebAppSetting.create web_app_title: 'Classifieds'
+
+      raise 'Unable to load web app settings' unless @web_app_settings
+    end
   end
 
   def sign_in
