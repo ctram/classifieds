1db0f865a3246252049a33b72043c2d04f34c367
Chris Tram <ctram@protonmail.com>
2019-09-01 14:57:19 -0700

    extract component from page

--- app/javascript/bundles/App/pages/ApplicationSettingsPage.jsx
+++ app/javascript/bundles/App/pages/ApplicationSettingsPage.jsx
@@ -1,65 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { setCurrentAlert } from '../actions/alertsActionCreators';
-import { updateWebAppSettings } from '../actions/webAppSettingsActionCreators';
+import ApplicationNameFormContainer from '../containers/ApplicationNameFormContainer';
 
-import { connect } from 'react-redux';
-
-class ApplicationSettingsPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.inputApplicationName = React.createRef();
-
-    this.updateApplicationName = this.updateApplicationName.bind(this);
-  }
-
-  updateApplicationName(e) {
-    e.preventDefault();
-
-    const webAppSettings = {
-      web_app_title: this.inputApplicationName.current.value
-    };
-
-    this.props.dispatch(updateWebAppSettings(webAppSettings));
-  }
-
+export default class ApplicationSettingsPage extends React.Component {
   render() {
-    const { currentUser, webAppSettings } = this.props;
-
     return <div className="d-flex flex-column align-items-center">
-      <h1 className="my-3">Application Settings</h1>
-
-      <div className="d-flex flex-column align-items-start">
-        <div className="my-3">
-          <h2 className="my-3">Application Name</h2>
-          <form onSubmit={this.updateApplicationName}>
-            <div className="form-group">
-              <input
-                id="application-name"
-                type="text"
-                className="form-control"
-                defaultValue={webAppSettings.web_app_title}
-                ref={this.inputApplicationName}
-                placeholder="Classifieds"
-                required />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Update Application Name
-            </button>
-          </form>
-        </div>
-      </div>
+      <ApplicationNameFormContainer {...this.props} />
     </div>;
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    currentUser: state.users.currentUser,
-    webAppSettings: state.webAppSettings
-  };
-};
-
-export default connect(mapStateToProps)(ApplicationSettingsPage);
