a5e6f53ed9195dc22bf79cb4ad098f46b21f88d5
Chris Tram <ctram@protonmail.com>
2019-09-01 17:36:23 -0700

    remove old table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_070237) do
+ActiveRecord::Schema.define(version: 2019_09_02_003504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "habits", force: :cascade do |t|
-    t.string "title"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.json "dates", default: {}
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
