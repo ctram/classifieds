ac2c65d1708ff4f61aa728494f5b723c9c60c611
Chris Tram <ctram@protonmail.com>
2019-09-03 00:28:40 -0700

    adds method to create classified_type_attributes for a classified_type

--- app/models/classified_type.rb
+++ app/models/classified_type.rb
@@ -1,6 +1,24 @@
+# frozen_string_literal: true
+
 class ClassifiedType < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :classified_type_attributes
   has_many :classifieds
+
+  def create_attributes(attributes)
+    return if attributes.empty?
+
+    ActiveRecord::Base.transaction do
+      attributes.each do |attr|
+        classified_type_attribute = classified_type_attributes.create(attr)
+
+        unless classified_type_attribute.valid?
+          raise classified_type_attribute.errors.messages
+        end
+      end
+    end
+
+    classified_type_attributes
+  end
 end
