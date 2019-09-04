4fc22facd238396987030f89971ac6564276a35e
Chris Tram <ctram@protonmail.com>
2019-09-03 20:53:55 -0700

    validate uniqueness of name of attribute within the classified_type

--- app/models/classified_type_attribute.rb
+++ app/models/classified_type_attribute.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class ClassifiedTypeAttribute < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :data_type, presence: true
 
+  validate :name_must_be_unique_within_classified_type
+
   belongs_to :classified_type
+
+  private
+
+  def name_must_be_unique_within_classified_type
+    classified_type.classified_type_attributes.any? do |attr|
+      if attr.name.downcase == name.downcase
+        errors.add(:name, 'must be unique among sibling classified_type_attributes of a classified_type')
+        return true
+      end
+    end
+  end
 end
