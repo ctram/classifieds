16b19456c3e27d8deaf889fcdc7941be3dd2922e
Chris Tram <ctram@protonmail.com>
2019-09-11 00:09:43 -0700

    adds endpoint to update user

--- app/api/classifieds_api/v1/user.rb
+++ app/api/classifieds_api/v1/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ClassifiedsAPI
   module V1
     class User < ClassifiedsAPI::V1::API
@@ -16,6 +18,40 @@ module ClassifiedsAPI
 
           present :user, user, using: Entities::User
           present :message, 'user_creation_success'
+        end
+
+        patch ':id' do
+          user_id = params[:id].to_i
+          
+          if user_id != current_user.id && current_user.role != 'admin'
+            error!('unauthorized', 401)
+          end
+
+          email, new_password, current_password = params[:user].values_at :email, :new_password, :current_password
+          user = ::User.find_by_id(user_id)
+
+          error!('error', 422) unless user
+
+          if current_user.role == 'admin'
+            error!('action_not_allowed_for_user', 403)
+          end
+
+          user.email = email if email
+
+          if new_password
+            unless user.authenticate(current_password) || current_user.role == 'admin'
+              error!('incorrect_password', 401)
+            end
+
+            user.password = new_password
+          end
+
+          unless user.save
+            error!({ error: 'user_update_error', detail: user.errors.messages }, 422)
+          end
+
+          present :user, user, with: Entities::User
+          present :message, 'user_update_success'
         end
       end
     end
