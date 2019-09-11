ec02ec219bde658f7e93b0d3fcf07454a936fd98
Chris Tram <ctram@protonmail.com>
2019-09-10 23:13:17 -0700

    stubs endpoints for web_app_settings

---
+++ app/api/classifieds_api/v1/web_app_setting.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ClassifiedsAPI
+  module V1
+    class WebAppSetting < ClassifiedsAPI::V1::API
+      resources :web_app_setting do
+        
+      end
+    end
+  end
+end
