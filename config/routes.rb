8420f0ae415c7d3b24aff98b1929acd275b0c9c1
Chris Tram <ctram@protonmail.com>
2019-09-11 00:15:19 -0700

    remove more routes pointing to controllers, prefer grape api

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
 
   patch '/web_app_settings', to: 'web_app_settings#update'
 
-  # resources :sessions, only: [:create]
-  # get '/sessions', to: 'sessions#authenticate'
-  # delete '/sessions', to: 'sessions#destroy'
-
   root 'statics#index'
-
-  get '/sign-in', to: 'statics#sign_in'
 
   mount ClassifiedsAPI::V1::API => '/'
 
