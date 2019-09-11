622c5425fd5b4ca2b27e816bf5f5749f45ef8f05
Chris Tram <ctram@protonmail.com>
2019-09-11 00:16:12 -0700

    clean up navbar, separate out logged-in versus logged-out links

--- app/javascript/bundles/App/components/NavBar.jsx
+++ app/javascript/bundles/App/components/NavBar.jsx
@@ -35,55 +35,55 @@ class NavBar extends React.Component {
   render() {
     const { currentUser, appName } = this.props;
 
-    const homePageTo = currentUser ? '/' : '/sign-in';
+    let domSignedInLinks = null;
+
+    if (currentUser) {
+      domSignedInLinks = (
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <span
+              className="nav-link font-weight-bold dropdown-toggle"
+              href="#"
+              data-toggle="dropdown"
+            >
+              {currentUser.email}
+            </span>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link className="dropdown-item" href="#" to="/user-settings" onClick={this.resetAlertBar}>User Settings</Link>
+              {
+                currentUser.role === 'admin'
+                  && <Link className="dropdown-item" href="#" to="/application-settings">Application Settings</Link>
+              }
+              <div className="dropdown-divider" />
+              <a className="dropdown-item" href="#" onClick={this.signOut}>Sign Out</a>
+            </div>
+          </li>
+        </ul>
+      );
+    }
+
+    const domSignedOutLinks = (
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <Link className="nav-link" href="#" to="/sign-in" onClick={this.resetAlertBar}>Sign In</Link>
+        </li>
+        <li className="nav-item active">
+          <Link className="nav-link" href="#" to="/sign-up" onClick={this.resetAlertBar}>Sign Up</Link>
+        </li>
+      </ul>
+    );
 
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" href="#" to={homePageTo} onClick={this.resetAlertBar}>{appName}</Link>
+        <Link className="navbar-brand" href="#" to="/" onClick={this.resetAlertBar}>{appName}</Link>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            {
-                currentUser && (
-                <li className="nav-item">
-                  <span
-                    className="nav-link font-weight-bold dropdown-toggle"
-                    href="#"
-                    data-toggle="dropdown"
-                  >
-                    {currentUser.email}
-                  </span>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <Link className="dropdown-item" href="#" to="/user-settings" onClick={this.resetAlertBar}>User Settings</Link>
-                    {
-                      currentUser.role === 'admin'
-                        && <Link className="dropdown-item" href="#" to="/application-settings">Application Settings</Link>
-                    }
-                    <div className="dropdown-divider" />
-                    <a className="dropdown-item" href="#" onClick={this.signOut}>Sign Out</a>
-                  </div>
-                </li>
-                )
-              }
-            {
-                !currentUser && (
-                <li className="nav-item active">
-                  <Link className="nav-link" href="#" to="/sign-in" onClick={this.resetAlertBar}>Sign In</Link>
-                </li>
-                )
-              }
-            {
-                !currentUser && (
-                <li className="nav-item active">
-                  <Link className="nav-link" href="#" to="/sign-up" onClick={this.resetAlertBar}>Sign Up</Link>
-                </li>
-                )
-              }
-          </ul>
+          { !currentUser && domSignedOutLinks}
+          { currentUser && domSignedInLinks}
         </div>
       </nav>
     );
