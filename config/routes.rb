70e583abc320989b1b8a4746062e690fec979fa7
Chris Tram <ctram@protonmail.com>
2019-09-10 18:44:37 -0700

    remove routes to rails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :update]
+  # resources :users, only: [:create, :update]
 
   patch '/web_app_settings', to: 'web_app_settings#update'
 
-  resources :sessions, only: [:create]
-  get '/sessions', to: 'sessions#authenticate'
-  delete '/sessions', to: 'sessions#destroy'
+  # resources :sessions, only: [:create]
+  # get '/sessions', to: 'sessions#authenticate'
+  # delete '/sessions', to: 'sessions#destroy'
 
   root 'statics#index'
 
   get '/sign-in', to: 'statics#sign_in'
 
-  mount ClassifiedsAPI::V1 => '/'
+  mount ClassifiedsAPI::V1::API => '/'
 
   get '*foo', to: 'statics#index'
 end
